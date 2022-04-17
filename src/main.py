b4acbe4978db3ab2d0345f66191dcc9f66db30eb
Jamie Lee <hello@jamiethalacker.dev>
2022-04-16 19:10:18 -0700

    Submissive command scaffolding

--- src/main.py
+++ src/main.py
@@ -1,19 +1,38 @@
 import click
 
 @click.group()
+@click.option('--override', help="Perform transaction on the base layer", is_flag=True)
 @click.version_option("0.1.0")
-def cli():
+def cli(override):
+    if override:
+        click.echo("Using base layer")
     """Layered Package Manager"""
 
 @cli.command()
-def sync():
+def install():
+    """Overlay additional packages"""
     click.echo("Test!")
 
 @cli.command()
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(name):
-    click.echo(f"Hello {name}!")
+def remove():
+    """Remove overlayed packages"""
+    click.echo("Test!")
+
+@cli.command()
+def search():
+    """Search package details for the given string"""
+    click.echo("Test!")
+
+@cli.command()
+def info():
+    """Display details about a package or group of packages"""
+    click.echo("Test!")
+
+@cli.command()
+def rollback():
+    """Revert to the previously booted tree.\n
+    With the override flag, this resets the base layer"""
+    click.echo("Test")
 
 def main():
     cli()
