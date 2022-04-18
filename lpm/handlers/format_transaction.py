0eef499f2fc05c57ab2bfffbf4743ce239a12ddd
Jamie Lee <hello@jamiethalacker.dev>
2022-04-18 01:07:22 -0700

    Update formatted transaction summary

--- lpm/handlers/format_transaction.py
+++ lpm/handlers/format_transaction.py
@@ -10,7 +10,7 @@ error_console = Console(stderr=True, style="bold red")
 base = dnf.base
 
 def format_transaction_summary():
-    if not base.transaction.install_set or base.transaction.remove_set:
+    if not base.transaction.install_set and not base.transaction.remove_set:
         error_console.print(f"No packages in transaction")
         exit()
 
@@ -18,6 +18,7 @@ def format_transaction_summary():
     colour = "green"
     total_download_size = 0
     total_installed_size = 0
+    transaction_type = ""
 
     if base.transaction.remove_set:
         colour = "red"
@@ -29,26 +30,31 @@ def format_transaction_summary():
     table.add_column("Size", justify="left", style="blue")
 
     if base.transaction.install_set:
+        transaction_type = "installed"
         table.add_row("[white]Packages to install:[/white]", "", "", "", "")
         table.add_row("", "", "", "", "")
         for pkg in base.transaction.install_set:
             table.add_row(pkg.name, pkg.version, pkg.arch, pkg.reponame, HumanBytes.format(pkg.downloadsize))
             total_download_size += pkg.downloadsize
             total_installed_size += pkg.installsize
-        table.add_row("", "", "", "", "")
-        table.add_row("[white]Total packages to install:[/white]", str(len(base.transaction.install_set)), "", "", "")
     if base.transaction.remove_set:
+        transaction_type = "removed"
         table.add_row("[white]Packages to remove:[/white]", "", "", "", "")
         table.add_row("", "", "", "", "")
         for pkg in base.transaction.remove_set:
             table.add_row(pkg.name, pkg.version, pkg.arch, pkg.reponame, HumanBytes.format(pkg.installsize))
-            total_installed_size += pkg.installedsize
-        table.add_row("", "", "", "", "")
-        table.add_row("[white]Total packages to remove:[/white]", str(len(base.transaction.remove_set)), "", "", "")
+            total_installed_size += pkg.installsize
 
     console.print(table)
 
-    console.print(f"Total Download Size: {HumanBytes.format(total_download_size)}")
-    console.print(f"Total Installed Size: {HumanBytes.format(total_installed_size)}")
+    if base.transaction.install_set:
+        console.print(f"Total packages to install: {str(len(base.transaction.install_set))}")
+
+    if base.transaction.remove_set:
+        console.print(f"Total packages to remove: {str(len(base.transaction.remove_set))}")
+
+    if total_download_size != 0:
+        console.print(f"Total download size: {HumanBytes.format(total_download_size)}")
+    console.print(f"Total {transaction_type} size: {HumanBytes.format(total_installed_size)}")
 
     click.confirm('Do you understand?', abort=True)
