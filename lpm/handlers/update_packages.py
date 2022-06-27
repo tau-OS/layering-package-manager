67f090bca792ff478dc26d509bdd5f2b5d158cad
Lleyton Gray <lleyton@fyralabs.com>
2022-06-26 23:36:43 -0400

    setup P2P connection

--- lpm/handlers/update_packages.py
+++ lpm/handlers/update_packages.py
@@ -1,3 +1,5 @@
+import dbus
+from dbus.connection import Connection
 from dbus_next import Variant
 from rich.console import Console
 from lpm import dnf
@@ -9,6 +11,20 @@ error_console = Console(stderr=True, style="bold red")
 base = dnf.base
 
 
+def new_transaction_peer_interface(address: str):
+    conn = Connection(address)
+
+    proxy = conn.get_object(
+        "org.projectatomic.rpmostree1",
+        "/",
+    )
+
+    return (
+        dbus.Interface(proxy, "org.projectatomic.rpmostree1.Transaction"),
+        dbus.Interface(proxy, "org.freedesktop.DBus.Properties"),
+    )
+
+
 async def update_packages():
     if not base.transaction.install_set and not base.transaction.remove_set:
         error_console.print(f"No packages in transaction")
@@ -16,21 +32,14 @@ async def update_packages():
 
     (interface, _) = await new_rpm_ostree_interface()
 
-    await interface.call_update_deployment(
-        {
-            "install-packages": Variant(
-                "as", list(map(lambda x: x.name, base.transaction.install_set))
-            ),
-            "remove-packages": Variant(
-                "as",
-                list(
-                    map(lambda x: x.name, base.transaction.remove_set),
-                ),
-            ),
-        },
+    peer_socket = await interface.call_pkg_change(
         {
             "initiating-command-line": Variant("s", "lpm"),
         },
+        list(map(lambda x: x.name, base.transaction.install_set)),
+        list(map(lambda x: x.name, base.transaction.remove_set)),
     )
 
-    await interface.call_finalize_deployment({})
+    (peer_interface, peer_properties) = new_transaction_peer_interface(peer_socket)
+
+    print(peer_properties.Get("org.projectatomic.rpmostree1.Transaction", "Title"))
