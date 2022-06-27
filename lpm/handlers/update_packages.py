ec6a86f781c82e02cf98558fba6f9116e3cf551f
Lleyton Gray <lleyton@fyralabs.com>
2022-06-27 00:30:33 -0400

    basic transaction crap

--- lpm/handlers/update_packages.py
+++ lpm/handlers/update_packages.py
@@ -1,3 +1,8 @@
+import threading
+from gi.repository import GLib
+import asyncio
+from dbus.mainloop.glib import DBusGMainLoop
+from typing import Tuple
 import dbus
 from dbus.connection import Connection
 from dbus_next import Variant
@@ -11,7 +16,13 @@ error_console = Console(stderr=True, style="bold red")
 base = dnf.base
 
 
+def run_glib_loop(loop: GLib.MainLoop):
+    loop.run()
+
+
 def new_transaction_peer_interface(address: str):
+    DBusGMainLoop(set_as_default=True)
+
     conn = Connection(address)
 
     proxy = conn.get_object(
@@ -42,4 +53,53 @@ async def update_packages():
 
     (peer_interface, peer_properties) = new_transaction_peer_interface(peer_socket)
 
-    print(peer_properties.Get("org.projectatomic.rpmostree1.Transaction", "Title"))
+    loop = GLib.MainLoop()
+    finished_event = asyncio.Event()
+
+    threading.Thread(target=run_glib_loop, args=(loop,)).start()
+
+    def finished_cb(success: bool, error_message: str):
+        print(error_message)
+        loop.quit()
+        finished_event.set()
+
+    def message_cb(text: str):
+        print(text)
+
+    def task_begin_cb(text: str):
+        pass
+
+    def task_end_cb(text: str):
+        pass
+
+    def percent_progress_cb(text: str, percentage: int):
+        pass
+
+    def download_progress_cb(
+        time: Tuple[int, int],
+        outstanding: Tuple[int, int],
+        metadata: Tuple[int, int, int],
+        delta: Tuple[int, int, int, int],
+        content: Tuple[int, int],
+        transfer: Tuple[int, int],
+    ):
+        pass
+
+    def signature_progress_db(signature: list[any], commit: str):
+        pass
+
+    def progress_end_db():
+        pass
+
+    peer_interface.connect_to_signal("Finished", finished_cb)
+    peer_interface.connect_to_signal("Message", message_cb)
+    peer_interface.connect_to_signal("TaskBegin", task_begin_cb)
+    peer_interface.connect_to_signal("TaskEnd", task_end_cb)
+    peer_interface.connect_to_signal("PercentProgress", percent_progress_cb)
+    peer_interface.connect_to_signal("DownloadProgress", download_progress_cb)
+    peer_interface.connect_to_signal("SignatureProgress", signature_progress_db)
+    peer_interface.connect_to_signal("ProgressEnd", progress_end_db)
+
+    print(peer_interface.Start())
+
+    await finished_event.wait()
