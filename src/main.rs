c9f54f68adce376bbdaffa19eb3d77d0622ea3b9
Lleyton Gray <lleyton@fyralabs.com>
2022-09-26 22:47:26 -0700

    Migrate to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![feature(unix_socket_abstract)]
 
 use clap::{Parser, Subcommand};
-use rpm_ostree::{OSProxyBlocking, SysrootProxyBlocking};
+use rpm_ostree::{OSProxy, SysrootProxy};
 use std::collections::HashMap;
-use zbus::blocking::Connection;
+use zbus::Connection;
 
 mod rpm_ostree;
 mod transaction;
@@ -24,52 +24,55 @@ enum Commands {
     Update {},
 }
 
-fn get_os_proxy<'a>(
-    sysroot: &'a SysrootProxyBlocking<'a>,
-) -> Result<OSProxyBlocking<'a>, zbus::Error> {
-    let os_path = sysroot.booted()?;
+async fn get_os_proxy<'a>(sysroot: &'a SysrootProxy<'a>) -> Result<OSProxy<'a>, zbus::Error> {
+    let os_path = sysroot.booted().await?;
 
-    let proxy = rpm_ostree::OSProxyBlocking::builder(sysroot.connection())
+    let proxy = rpm_ostree::OSProxy::builder(sysroot.connection())
         .destination("org.projectatomic.rpmostree1")?
         .path(os_path)?
-        .build()?;
+        .build()
+        .await?;
 
     Ok(proxy)
 }
 
-#[async_std::main]
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
 
-    let connection = Connection::system()?;
+    let connection = Connection::system().await?;
     match &cli.command {
         Commands::Install { name } => {
-            let sysroot = rpm_ostree::SysrootProxyBlocking::new(&connection)?;
-            let proxy = get_os_proxy(&sysroot)?;
+            let sysroot = rpm_ostree::SysrootProxy::new(&connection).await?;
+            let proxy = get_os_proxy(&sysroot).await?;
 
             let packages = name.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
-            let transaction_address = proxy.pkg_change(HashMap::new(), packages.as_slice(), &[])?;
+            let transaction_address = proxy
+                .pkg_change(HashMap::new(), packages.as_slice(), &[])
+                .await?;
 
             let transaction = transaction::get_transaction(&transaction_address).await?;
             transaction.start().await?;
             transaction::handle_transaction(&transaction).await?;
         }
         Commands::Remove { name } => {
-            let sysroot = rpm_ostree::SysrootProxyBlocking::new(&connection)?;
-            let proxy = get_os_proxy(&sysroot)?;
+            let sysroot = rpm_ostree::SysrootProxy::new(&connection).await?;
+            let proxy = get_os_proxy(&sysroot).await?;
 
             let packages = name.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
-            let transaction_address = proxy.pkg_change(HashMap::new(), &[], packages.as_slice())?;
+            let transaction_address = proxy
+                .pkg_change(HashMap::new(), &[], packages.as_slice())
+                .await?;
 
             let transaction = transaction::get_transaction(&transaction_address).await?;
             transaction.start().await?;
             transaction::handle_transaction(&transaction).await?;
         }
         Commands::Update {} => {
-            let sysroot = rpm_ostree::SysrootProxyBlocking::new(&connection)?;
-            let proxy = get_os_proxy(&sysroot)?;
+            let sysroot = rpm_ostree::SysrootProxy::new(&connection).await?;
+            let proxy = get_os_proxy(&sysroot).await?;
 
-            let transaction_address = proxy.upgrade(HashMap::new())?;
+            let transaction_address = proxy.upgrade(HashMap::new()).await?;
 
             let transaction = transaction::get_transaction(&transaction_address).await?;
             transaction.start().await?;
--- src/transaction.rs
+++ src/transaction.rs
@@ -1,7 +1,4 @@
 use crate::rpm_ostree::TransactionProxy;
-use async_signals::Signals;
-use async_std::stream::StreamExt;
-use futures_util::join;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::{
     cell::RefCell,
@@ -9,192 +6,206 @@ use std::{
     process::exit,
     time::Duration,
 };
+use tokio::try_join;
+use tokio_stream::StreamExt;
 
-pub async fn handle_transaction(transaction: &TransactionProxy<'_>) -> Result<(), zbus::Error> {
-    let mut receive_message = transaction.receive_message().await?;
-    let mut receive_percent_progress = transaction.receive_percent_progress().await?;
-    let mut receive_download_progress = transaction.receive_download_progress().await?;
-    let mut receive_progress_end = transaction.receive_progress_end().await?;
-    let mut receive_task_begin = transaction.receive_task_begin().await?;
-    let mut receive_task_end = transaction.receive_task_end().await?;
-    let mut receive_finished = transaction.receive_finished().await?;
-
-    let mut signals = Signals::new(vec![2, 15]).unwrap();
-
+pub async fn handle_transaction(transaction: &TransactionProxy<'_>) -> anyhow::Result<()> {
     let bar: RefCell<Option<ProgressBar>> = RefCell::new(None);
 
-    join!(
+    let result: anyhow::Result<_> = try_join!(
         async {
-            while let Some(_) = receive_message.next().await {
-                // let args = message.args().unwrap();
-                // println!("{}", args.text);
+            let mut receive_message = transaction.receive_message().await?;
+
+            while let Some(message) = receive_message.next().await {
+                let args = message.args()?;
+                println!("{}", args.text);
             }
+
+            Ok(())
         },
         async {
+            let mut receive_percent_progress = transaction.receive_percent_progress().await?;
+
             while let Some(message) = receive_percent_progress.next().await {
                 if bar.borrow().is_none() {
                     let new_bar = indicatif::ProgressBar::new(100);
                     new_bar.set_style(
                         ProgressStyle::with_template(&format!(
                             "{{prefix:.bold}}{{bar:20}} {{msg}}"
-                        ))
-                        .unwrap()
+                        ))?
                         .progress_chars("█▓▒░  "),
                     );
                     bar.replace(Some(new_bar));
                 }
 
-                let args = message.args().unwrap();
+                let args = message.args()?;
                 let text = args.text.to_string();
                 let bar = bar.borrow();
                 let bar = bar.as_ref().unwrap();
                 bar.set_position(args.percentage.into());
                 bar.set_message(text);
             }
+
+            Ok(())
         },
         async {
+            let mut receive_download_progress = transaction.receive_download_progress().await?;
             while let Some(_) = receive_download_progress.next().await {
                 // println!("{:#?}", message.args().unwrap().time());
             }
+
+            Ok(())
         },
         async {
+            let mut receive_progress_end = transaction.receive_progress_end().await?;
             while let Some(_message) = receive_progress_end.next().await {
                 let old_bar = bar.take().unwrap();
                 old_bar.finish();
             }
+
+            Ok(())
         },
         async {
+            let mut receive_task_begin = transaction.receive_task_begin().await?;
             while let Some(message) = receive_task_begin.next().await {
                 let args = message.args().unwrap();
                 let new_bar = indicatif::ProgressBar::new_spinner();
-                new_bar.enable_steady_tick(Duration::from_millis(120));
+                new_bar.enable_steady_tick(Duration::from_millis(17));
                 new_bar.set_style(
-                    ProgressStyle::with_template("{spinner} {msg}")
-                        .unwrap()
-                        .tick_strings(&[
-                            "█▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "███▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "██████▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "██████▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "███████▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "████████▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "██████████▁▁▁▁▁▁▁▁▁▁",
-                            "███████████▁▁▁▁▁▁▁▁▁",
-                            "█████████████▁▁▁▁▁▁▁",
-                            "██████████████▁▁▁▁▁▁",
-                            "██████████████▁▁▁▁▁▁",
-                            "▁██████████████▁▁▁▁▁",
-                            "▁██████████████▁▁▁▁▁",
-                            "▁██████████████▁▁▁▁▁",
-                            "▁▁██████████████▁▁▁▁",
-                            "▁▁▁██████████████▁▁▁",
-                            "▁▁▁▁█████████████▁▁▁",
-                            "▁▁▁▁██████████████▁▁",
-                            "▁▁▁▁██████████████▁▁",
-                            "▁▁▁▁▁██████████████▁",
-                            "▁▁▁▁▁██████████████▁",
-                            "▁▁▁▁▁██████████████▁",
-                            "▁▁▁▁▁▁██████████████",
-                            "▁▁▁▁▁▁██████████████",
-                            "▁▁▁▁▁▁▁█████████████",
-                            "▁▁▁▁▁▁▁█████████████",
-                            "▁▁▁▁▁▁▁▁████████████",
-                            "▁▁▁▁▁▁▁▁████████████",
-                            "▁▁▁▁▁▁▁▁▁███████████",
-                            "▁▁▁▁▁▁▁▁▁███████████",
-                            "▁▁▁▁▁▁▁▁▁▁██████████",
-                            "▁▁▁▁▁▁▁▁▁▁██████████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁████████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁██████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
-                            "█▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
-                            "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
-                            "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
-                            "███▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
-                            "████▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
-                            "█████▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "█████▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "██████▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "████████▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "█████████▁▁▁▁▁▁▁▁▁▁▁",
-                            "███████████▁▁▁▁▁▁▁▁▁",
-                            "████████████▁▁▁▁▁▁▁▁",
-                            "████████████▁▁▁▁▁▁▁▁",
-                            "██████████████▁▁▁▁▁▁",
-                            "██████████████▁▁▁▁▁▁",
-                            "▁██████████████▁▁▁▁▁",
-                            "▁██████████████▁▁▁▁▁",
-                            "▁▁▁█████████████▁▁▁▁",
-                            "▁▁▁▁▁████████████▁▁▁",
-                            "▁▁▁▁▁████████████▁▁▁",
-                            "▁▁▁▁▁▁███████████▁▁▁",
-                            "▁▁▁▁▁▁▁▁█████████▁▁▁",
-                            "▁▁▁▁▁▁▁▁█████████▁▁▁",
-                            "▁▁▁▁▁▁▁▁▁█████████▁▁",
-                            "▁▁▁▁▁▁▁▁▁█████████▁▁",
-                            "▁▁▁▁▁▁▁▁▁▁█████████▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁████████▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁████████▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁███████▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁███████▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                            "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
-                        ]),
+                    ProgressStyle::with_template("{spinner} {msg}")?.tick_strings(&[
+                        "█▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "███▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "████▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "██████▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "██████▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "███████▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "████████▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "██████████▁▁▁▁▁▁▁▁▁▁",
+                        "███████████▁▁▁▁▁▁▁▁▁",
+                        "█████████████▁▁▁▁▁▁▁",
+                        "██████████████▁▁▁▁▁▁",
+                        "██████████████▁▁▁▁▁▁",
+                        "▁██████████████▁▁▁▁▁",
+                        "▁██████████████▁▁▁▁▁",
+                        "▁██████████████▁▁▁▁▁",
+                        "▁▁██████████████▁▁▁▁",
+                        "▁▁▁██████████████▁▁▁",
+                        "▁▁▁▁█████████████▁▁▁",
+                        "▁▁▁▁██████████████▁▁",
+                        "▁▁▁▁██████████████▁▁",
+                        "▁▁▁▁▁██████████████▁",
+                        "▁▁▁▁▁██████████████▁",
+                        "▁▁▁▁▁██████████████▁",
+                        "▁▁▁▁▁▁██████████████",
+                        "▁▁▁▁▁▁██████████████",
+                        "▁▁▁▁▁▁▁█████████████",
+                        "▁▁▁▁▁▁▁█████████████",
+                        "▁▁▁▁▁▁▁▁████████████",
+                        "▁▁▁▁▁▁▁▁████████████",
+                        "▁▁▁▁▁▁▁▁▁███████████",
+                        "▁▁▁▁▁▁▁▁▁███████████",
+                        "▁▁▁▁▁▁▁▁▁▁██████████",
+                        "▁▁▁▁▁▁▁▁▁▁██████████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁████████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁██████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
+                        "█▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
+                        "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
+                        "██▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
+                        "███▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
+                        "████▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
+                        "█████▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "█████▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "██████▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "████████▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "█████████▁▁▁▁▁▁▁▁▁▁▁",
+                        "███████████▁▁▁▁▁▁▁▁▁",
+                        "████████████▁▁▁▁▁▁▁▁",
+                        "████████████▁▁▁▁▁▁▁▁",
+                        "██████████████▁▁▁▁▁▁",
+                        "██████████████▁▁▁▁▁▁",
+                        "▁██████████████▁▁▁▁▁",
+                        "▁██████████████▁▁▁▁▁",
+                        "▁▁▁█████████████▁▁▁▁",
+                        "▁▁▁▁▁████████████▁▁▁",
+                        "▁▁▁▁▁████████████▁▁▁",
+                        "▁▁▁▁▁▁███████████▁▁▁",
+                        "▁▁▁▁▁▁▁▁█████████▁▁▁",
+                        "▁▁▁▁▁▁▁▁█████████▁▁▁",
+                        "▁▁▁▁▁▁▁▁▁█████████▁▁",
+                        "▁▁▁▁▁▁▁▁▁█████████▁▁",
+                        "▁▁▁▁▁▁▁▁▁▁█████████▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁████████▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁████████▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁███████▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁███████▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁███████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁████",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁███",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁██",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁█",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                        "▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁",
+                    ]),
                 );
                 new_bar.set_message(args.text.to_string());
 
                 bar.replace(Some(new_bar));
-                // println!("{}", args.text);
             }
+
+            Ok(())
         },
         async {
+            let mut receive_task_end = transaction.receive_task_end().await?;
             while let Some(_) = receive_task_end.next().await {
-                let bar = bar.take().unwrap();
-                bar.finish();
+                if let Some(bar) = bar.take() {
+                    bar.finish();
+                }
             }
+
+            Ok(())
         },
         async {
+            let mut receive_finished = transaction.receive_finished().await?;
             let message = receive_finished.next().await.unwrap();
-            let args = message.args().unwrap();
+            let args = message.args()?;
             if !args.success {
                 println!("{}", args.error_message);
                 exit(1)
             } else {
                 exit(0)
             }
+
+            Ok(())
         },
         async {
-            while let Some(signal) = signals.next().await {
-                println!("Signal: {}", signal);
-                transaction.cancel().await.unwrap();
-            }
+            tokio::signal::ctrl_c().await?;
+            transaction.cancel().await?;
+
+            Ok(())
         }
     );
+
+    result?;
 
     Ok(())
 }
@@ -206,7 +217,11 @@ pub async fn get_transaction(transaction_address: &str) -> Result<TransactionPro
             .expect("Failed to parse Unix stream connection address")
             .as_bytes(),
     )?;
-    let stream = UnixStream::connect_addr(&addr)?;
+    let stream = {
+        let std_stream = UnixStream::connect_addr(&addr)?;
+        std_stream.set_nonblocking(true)?;
+        tokio::net::UnixStream::from_std(std_stream)?
+    };
 
     let transaction_connection = zbus::ConnectionBuilder::unix_stream(stream)
         .p2p()
